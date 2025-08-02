e642546b8a7bb9af926f0f87060c766b80359a34
shujaat mallick <shjujaatmallick@gmail.com>
2025-08-02 17:11:32 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1156,6 +1156,8 @@ const App = () => {
               </motion.div>
 
               <motion.button
+              href="\resume.pdf"
+                download="Shujaat_Mallick_Resume.pdf"
                 variants={itemVariants}
                 className="group bg-gradient-to-r from-purple-500 to-pink-500 px-8 py-4 rounded-full font-semibold flex items-center gap-3 text-sm uppercase tracking-wider shadow-xl hover:shadow-purple-500/30 transition-all duration-300"
                 whileHover={{ scale: 1.05, y: -2 }}
